3741b964842ebef979e4d268dd519b0ade3a5ad3
Aditya Kumar <aditya.kumar@autoplant.in>
2023-09-25 15:15:37 +0530

    Initialized Files

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 import React from 'react'
-import { getDatabase, ref, set } from "firebase/database";
-import { app } from "./firebase";
-import Signup from './containers/Signup/Signup';
-import { useFirebase } from './context/Firebase';
-
-const db = getDatabase(app);
 
 const App = () => {
-
-  const firebase = useFirebase();
-
-  console.log(firebase);
-  const putData = () => {
-    set(ref(db, "users/jay"), {
-      id: 1,
-      name: "Jay",
-      age: 21,
-    });
-  }
   return (
     <div>
       Track Your Expenses
-
-      <button onClick={putData}>Put Data</button>
-
-      <Signup />
     </div>
   )
 }
