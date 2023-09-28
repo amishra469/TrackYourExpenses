11fbdd8ef1fe2ce28f64ed3d0f5904f37094d20d
amishra469 <svmaditya01@gmail.com>
2023-09-28 19:11:08 +0530

    Added Top Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./context/Theme";
+import TopNavBar from './components/TopNavBar/TopNavBar';
 
 const App = () => {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          {/* <Sidebar isSidebar={isSidebar} /> */}
+          <main className="content">
+            <TopNavBar setIsSidebar={setIsSidebar} />
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
